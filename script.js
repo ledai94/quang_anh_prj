f2576669208c7c78faf23239369bf3d6505bbee4
ledai494 <brian3dcnme@gmail.com>
2022-08-30 20:42:08 +0700

    update hover active play nơ

--- script.js
+++ script.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
   $(".closebtn,.js-hide-navbar-mobile,.overlay").on("click", function () {
     $(".overlay, .navbar-mobile, .overlay-content").removeClass("active");
   });
+  // handle btn event
+  $(".navbar-story").mouseenter(function () {
+    $(".navbar-story img").attr("src", function (index, attr) {
+      return attr.replace("_Normal.png", "_Hover.png");
+    });
+  });
+  $(".navbar-story").mouseleave(function () {
+    $(".navbar-story img").attr("src", function (index, attr) {
+      return attr.replace("_Hover.png", "_Normal.png");
+    });
+  });
 });
