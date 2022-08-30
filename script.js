f23d660ae2d2713048e6a457a73f7f1a6868f000
ledai494 <brian3dcnme@gmail.com>
2022-08-30 07:28:36 +0700

    update slide show

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  $(".block-game-play__slideshow").slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-  });
+  // $(".block-game-play__slideshow").slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   centerMode: true,
+  //   variableWidth: true,
+  // });
   // action sidenav menu overlay
   $(".js-show-navbar-mobile").on("click", function () {
     $(".overlay, .navbar-mobile, .overlay-content").toggleClass("active");
