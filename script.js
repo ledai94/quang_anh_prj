24eb7b249241610ef22a55112c013ee942d1fec7
ledai494 <brian3dcnme@gmail.com>
2022-08-30 21:14:08 +0700

    update

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     $(".overlay, .navbar-mobile, .overlay-content").removeClass("active");
   });
   // handle btn event
+
   $(".navbar-story").mouseenter(function () {
     $(".navbar-story img").attr("src", function (index, attr) {
       return attr.replace("_Normal.png", "_Hover.png");
@@ -15,6 +16,39 @@ $(document).ready(function () {
   });
   $(".navbar-story").mouseleave(function () {
     $(".navbar-story img").attr("src", function (index, attr) {
+      return attr.replace("_Hover.png", "_Normal.png");
+    });
+  });
+
+  $(".navbar-game-play").mouseenter(function () {
+    $(".navbar-game-play img").attr("src", function (index, attr) {
+      return attr.replace("_Normal.png", "_Hover.png");
+    });
+  });
+  $(".navbar-game-play").mouseleave(function () {
+    $(".navbar-game-play img").attr("src", function (index, attr) {
+      return attr.replace("_Hover.png", "_Normal.png");
+    });
+  });
+
+  $(".navbar-feature").mouseenter(function () {
+    $(".navbar-feature img").attr("src", function (index, attr) {
+      return attr.replace("_Normal.png", "_Hover.png");
+    });
+  });
+  $(".navbar-feature").mouseleave(function () {
+    $(".navbar-feature img").attr("src", function (index, attr) {
+      return attr.replace("_Hover.png", "_Normal.png");
+    });
+  });
+
+  $(".navbar-roadmap").mouseenter(function () {
+    $(".navbar-roadmap img").attr("src", function (index, attr) {
+      return attr.replace("_Normal.png", "_Hover.png");
+    });
+  });
+  $(".navbar-roadmap").mouseleave(function () {
+    $(".navbar-roadmap img").attr("src", function (index, attr) {
       return attr.replace("_Hover.png", "_Normal.png");
     });
   });
